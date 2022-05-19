aee3fe75fbc0e956312f452dbb62707839dad053
sergera <sergera88@gmail.com>
2022-05-19 14:13:42 -0300

    More Slice Utilities
    - Added "Find", "Map", and "RemoveByIndex" slice utilities to slc package

--- pkg/slc/slc.go
+++ pkg/slc/slc.go
@@ -12,13 +12,38 @@ func ShallowEqual[T comparable](a, b []T) bool {
 	return true
 }
 
-func Filter[T any](slc []T, f func(T) bool) []T {
-	fltd := make([]T, 0)
+func Filter[T any](slc []T, f func(T) bool) (fltd []T) {
 	for _, e := range slc {
 		if f(e) {
 			fltd = append(fltd, e)
 		}
 	}
 
-	return fltd
+	return
+}
+
+func Map[T any](slc []T, f func(T) T) []T {
+	mapped := make([]T, len(slc))
+
+	for i, e := range slc {
+		mapped[i] = f(e)
+	}
+
+	return mapped
+}
+
+func Find[T any](slc []T, f func(T) bool) (fnd T, isFnd bool) {
+	for _, e := range slc {
+		if f(e) {
+			fnd = e
+			isFnd = true
+			return
+		}
+	}
+
+	return
+}
+
+func RemoveByIndex[T any](slc []T, idx int) []T {
+	return append(slc[:idx], slc[idx+1:]...)
 }
