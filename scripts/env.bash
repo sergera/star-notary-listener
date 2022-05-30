99973e115f76f30893c8599f58885a8594c4913e
sergera <sergera88@gmail.com>
2022-05-30 19:00:43 -0300

    Env Script Error Messages
    - Added "error:" to the beggining of messages in conditions with non-zero
    exit code to improve user feedback comprehension
    
    - Added a more meaningful message in the case that the 'env' file is not found
    
    - Added line number to empty variable name error message
    
    - Added variable name to invalid variable value error message
    
    - Fixed documentation indentation

--- scripts/env.bash
+++ scripts/env.bash
@@ -2,55 +2,58 @@
 
 # Author: Sergio Joselli
 # Created: 30th May 2022
-# Last Modified: 30th May 2022
+# Last Modified: 31th May 2022
 
 # Description:
 # Exports environment variables defined in a file named "env" in project root
 # Each line of the env file must be as follows
 #   ENV_VARIABLE_NAME=ENV_VARIABLE_VALUE with an optional NEWLINE in the end (\n)
-#		ENV_VARIABLE_NAME accepts upper case letters (A-Z) and underscores (_)
-#		ENV_VARIABLE_VALUE accepts upper and lower case letters (A-Za-z), numbers (0-9), 
-#		underscores (_), forward slashes (/), colons (:), full stops (.), commas (,)
-#		and dashes (-)
+#   ENV_VARIABLE_NAME accepts upper case letters (A-Z) and underscores (_)
+#   ENV_VARIABLE_VALUE accepts upper and lower case letters (A-Za-z), numbers (0-9),
+#   underscores (_), forward slashes (/), colons (:), full stops (.), commas (,)
+#   and dashes (-)
 
 # Usage:
 # source env.bash
 
-echo "setting env variables"
+echo "setting env variables..."
 
 source $(dirname ${BASH_SOURCE[0]})/root_path.bash
 env_path=$root_path/env
 
 if [[ !(-f $env_path) ]]; then
-	echo "env file not found"
+	echo "error: env file not found"
+	echo "there must be a file named 'env' in the project root directory"
 	exit 1
 fi
 
 allowed_name="^[A-Z_]+$"
 allowed_value="^[0-9A-Za-z_/:.,\-]+$"
 
+line_counter=0
 while IFS= read -r line || [ -n "$line" ]; do
+	line_counter=$(( $line_counter + 1 ))
 	line_arr=($(echo $line | tr "=" "\n"))
 	var_name=$(echo ${line_arr[0]} | tr -d "\n")
 	var_value=$(echo ${line_arr[1]} | tr -d "\n")
 
 	if [[ -z $var_name ]]; then
-		echo "empty variable name"
+		echo "error: empty variable name on line $line_counter"
 		exit 1
 	fi
 
 	if [[ -z $var_value ]]; then
-		echo "empty variable value for $var_name"
+		echo "error: empty variable value for $var_name"
 		exit 1
 	fi
 
 	if [[ !($var_name =~ $allowed_name) ]]; then
-		echo "invalid variable name: $var_name"
+		echo "error: invalid variable name $var_name"
 		exit 1
 	fi
 
 	if [[ !($var_value =~ $allowed_value) ]]; then
-		echo "invalid variable value: $var_value"
+		echo "error: invalid variable value $var_value for $var_name"
 		exit 1
 	fi
 
