f65b4444f4678e6553a8429f587a7ac5dddf6e93
sergera <sergera88@gmail.com>
2022-06-01 18:33:41 -0300

    Check Module Version Script
    - Added script to check if a module with the provided path and version is found
    in go modules cache
    
    - Changed abigen installation script to use the new script
    
    - Added documentation

---
+++ scripts/check_mod_ver.bash
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Author: Sergio Joselli
+# Created: 1th June 2022
+# Last Modified: 1th June 2022
+
+# Description:
+# Checks if go module is installed in GOPATH with the requested version
+# Prints version to stdout if a module with the version is found in GOPATH
+#
+# It receives two positional parameters
+#   1- the full go module path
+#   2- the requested version
+#
+# Fails if
+#   1- parameters are not provided
+#   2- module path parameter contains illegal characters
+#   3- version parameter not compatible with semver
+#   e.g. version parameter should be x.x.x with optional concatenated tags -y 
+#        where x is numeric and y is alphanumberic
+#   4- module with requested version not found in GOPATH
+# In case of failure it prints the error message to stdout
+
+# Usage:
+# check_mod_ver.bash MODULE_PATH VERSION
+#
+# Typically stdout is assigned to a variable
+# result=$(check_mod_ver.bash github.com/someorg/somemodule 1.0.0) && {
+#	  version=$result
+# } || {
+#	  error=$result
+# }
+
+module_path=$1
+[[ -z $module_path ]] && {
+	echo "error: missing module path positional parameter"
+	exit 1
+}
+
+requested_version=$2
+[[ -z $requested_version ]] && {
+	echo "error: missing version positional parameter"
+	exit 1
+}
+
+illegal_module_path_char='^.*[][,;\!@#$%¨&*=+`´|(){}<>"'\''
+ 	]+.*$'
+[[ $module_path =~ $illegal_module_path_char ]] && {
+	echo "error: illegal go module path characters in parameter $module_path"
+	exit 1
+}
+
+semver_format='^[0-9]+\.[0-9]+\.[0-9]+(?:-[0-9a-z]+)*$'
+[[ $requested_version =~ $semver_format ]] && {
+	echo "error: invalid semver format in parameter $requested_version"
+	exit 1
+}
+
+this_dir=$(dirname "${BASH_SOURCE[0]}")
+go_paths_result=($($this_dir/get_go_paths.bash)) && {
+	go_root=${go_paths_result[0]}
+	go_path=${go_paths_result[1]}
+} || {
+	echo ${go_paths_result[*]}
+	exit 1
+}
+
+if [[ !($(compgen -G $go_path"/pkg/mod/$module_path*")) ]]; then
+	echo "error: $module_path not found in go modules cache"
+	exit 1
+fi
+
+for file in "$go_path"/pkg/mod/$module_path*; do
+	currently_read_version=${file##*@v}
+
+	[[ $currently_read_version = $requested_version ]] && {
+		echo $currently_read_version
+		exit 0
+	}
+done
+
+echo "error: module $module_path with version $requested_version not found"
+exit 1
--- scripts/install_abigen.bash
+++ scripts/install_abigen.bash
@@ -13,10 +13,8 @@
 # It runs "go install" in the "abigen" directory of the geth main package
 #
 # The installation will only happen if
-#   1- go root binary directory is in the $PATH environment variable
-#   2- the "go env GOPATH" command returns a non-empty string
-#   3- abigen binary with the same version as the geth version used in project is not in GOBIN
-#   4- geth module with the same version as the geth version used in project is in go modules cache
+#   1- abigen binary with the same version as the geth version used in project is not in GOBIN
+#   2- geth module with the same version as the geth version used in project is in go modules cache
 
 # Usage:
 # install_abigen.bash
@@ -33,36 +31,22 @@ go_paths_result=($($this_dir/get_go_paths.bash)) && {
 	exit 1
 }
 
-geth_version_result=$($this_dir/get_dependency_ver.bash go-ethereum) && {
-	project_geth_version=$geth_version_result
+dependency_version_result=$($this_dir/get_dependency_ver.bash go-ethereum) && {
+	project_geth_version=$dependency_version_result
 	echo "project using geth version $project_geth_version"
 } || {
-	echo $geth_version_result
+	echo $dependency_version_result
 	exit 1
 }
 
-function check_geth_module_versions() {
-	if [[ !($(compgen -G $go_path"/pkg/mod/github.com/ethereum/go-ethereum*")) ]]; then
-		echo "error: geth not found in go modules cache"
+function check_geth_module_version() {
+	geth_version_result=$($this_dir/check_mod_ver.bash github.com/ethereum/go-ethereum $project_geth_version) && {
+		echo "geth version $geth_version_result found in modules cache"
+	} || {
+		echo "error: geth version $project_geth_version not found in go modules cache"
 		echo "please run: go mod tidy"
 		exit 1
-	fi
-
-	gopath_has_project_version=false
-	for file in "$go_path"/pkg/mod/github.com/ethereum/go-ethereum*; do
-		currently_read_geth_version=${file#*@v}
-		echo "found geth version $currently_read_geth_version in go modules cache"
-
-		if [[ $project_geth_version = $currently_read_geth_version ]]; then
-			gopath_has_project_version=true
-		fi
-	done
-
-	if [[ !($gopath_has_project_version = true) ]]; then
-		echo "error: geth version used in project not detected in go modules cache"
-		echo "please run: go mod tidy"
-		exit 1
-	fi
+	}
 }
 
 function install() {
@@ -81,14 +65,14 @@ if [[ -x $go_path/bin/abigen ]]; then
 		exit 0
 		else
 		echo "abigen version differs from geth version used in project"
-		check_geth_module_versions
+		check_geth_module_version
 		echo "overwriting abigen binary..."
 		install && exit 0
 	fi
 
 	else
 	echo "abigen not detected in go binaries"
-	check_geth_module_versions
+	check_geth_module_version
 	echo "adding abigen binary..."
 	install && exit 0
 fi
